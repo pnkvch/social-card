a54aabb2ae8eabb47f5755b9fedf3dc6b7242e2f
apinkev <apinkevich@yahoo.com>
2019-12-30 23:44:56 +0200

    adding mock data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SocialCard from './socialCard';
 
 function App() {
   return (
     <div className="App">
-      Social Card
+      <SocialCard />
     </div>
   );
 }
---
+++ src/socialCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import data from './MOCK_DATA.json'
+
+const SocialCard = () => {
+    return (
+        <div>
+            {data[0].id}
+        </div>
+    )
+}
+
+export default SocialCard
