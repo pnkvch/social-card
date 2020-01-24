27ef6b4502204eba65a5cdf49d0603f186229a1a
apinkev <apinkevich@yahoo.com>
2020-01-24 14:28:16 +0100

    bottom part and small tweaks

--- src/socialCard/bottomSocial.js
+++ src/socialCard/bottomSocial.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faRetweet,
+    faHeart,
+    faComment,
+    faEnvelope
+} from "@fortawesome/free-solid-svg-icons";
+import "./bottomSocial.css";
 
-const BottomSocial = () => {
+const BottomSocial = props => {
+    const { likes, retweets } = props;
+    const [likeState, setLikeState] = useState(false);
+    const [retweetState, setRetweetState] = useState(false);
+
+    const onChange = (state, setState, type) => {
+        if (state) {
+            document
+                .querySelector(`.${type}-container`)
+                .classList.remove(`${type}-active`);
+            setState(false);
+        } else {
+            document
+                .querySelector(`.${type}-container`)
+                .classList.add(`${type}-active`);
+            setState(true);
+        }
+    };
+
     return (
-        <div>bottom</div>
-    )
-}
-export default BottomSocial
+        <div className="bottom-social">
+            <div className="bottom-wrapper">
+                <div className="icon-wrapper">
+                    <div>
+                        <FontAwesomeIcon icon={faComment} />
+                        <span>2</span>
+                    </div>
+                    <div
+                        onClick={() => {
+                            onChange(retweetState, setRetweetState, `retweet`);
+                        }}
+                        className="retweet-container"
+                    >
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <span>{retweets}</span>
+                    </div>
+                    <div
+                        onClick={() => {
+                            onChange(likeState, setLikeState, `like`);
+                        }}
+                        className="like-container"
+                    >
+                        <FontAwesomeIcon icon={faHeart} />
+                        <span>{likes}</span>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default BottomSocial;
